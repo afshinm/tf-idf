9f03682ff85910b2769e28d141492e17783a8eb1
Afshin Mehrabani <afshin.meh@gmail.com>
2017-03-28 19:45:41 +0430

    adding tfidf method, adding docs and finishing testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,4 @@
-mod tfidf;
+#![crate_type = "lib"]
+#![crate_name = "tfidf"]
 
-#[cfg(test)]
-mod tests {
-    use tfidf::TfIdf;
-    use tfidf::Term;
-
-    fn setup<'a>() -> TfIdf<'a> {
-        let mut tfidf = TfIdf::new();
-        tfidf.add("hello hello hello world");
-
-        tfidf
-    }
-
-    #[test]
-    fn test_tf() {
-        let tfidf = setup();
-
-        assert_eq!(tfidf.tf(&Term("hello"), 0), 1.4771212f32);
-        assert_eq!(tfidf.tf(&Term("world"), 0), 1f32);
-        assert_eq!(tfidf.tf(&Term("nothing"), 0), 0f32);
-    }
-}
+pub mod tfidf;
--- src/tfidf.rs
+++ src/tfidf.rs
@@ -1,12 +1,3 @@
-/// Represents a document and its associated score
-#[derive(Debug)]
-pub struct WeightedTerm<'a> {
-    /// Document
-    pub value: &'a str,
-    /// Score of document
-    pub score: f32
-}
-
 /// Represents a term
 #[derive(Debug)]
 pub struct Term<'a>(pub &'a str);
@@ -24,14 +15,46 @@ impl<'a> PartialEq for Term<'a> {
 }
 
 impl<'a> TfIdf<'a> {
+    /// Returns a TF-IDF object
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// let tfidf = TfIdf::new();
+    /// ```
     pub fn new() -> TfIdf<'a> {
         TfIdf { documents: vec![] }
     }
 
+    /// Adding a vector of Terms
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// use tfidf::tfidf::Term;
+    ///
+    /// let mut terms: Vec<Term> = vec![];
+    /// terms.push(Term("Hello world"));
+    ///
+    /// let mut tfidf = TfIdf::new();
+    /// tfidf.add_vec(terms);
+    /// ```
     pub fn add_vec(&mut self, document: Vec<Term<'a>>) {
         self.documents.push(document);
     }
 
+    /// Adding a new document
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    ///
+    /// let mut tfidf = TfIdf::new();
+    /// tfidf.add("hello world");
+    /// ```
     pub fn add(&mut self, document: &'a str) {
         let mut terms: Vec<Term<'a>> = vec![];
 
@@ -42,13 +65,68 @@ impl<'a> TfIdf<'a> {
         self.add_vec(terms);
     }
 
-    //pub fn idf(&self, term: &Term, )
+    /// Returns the count of documents containing a specific Term
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// use tfidf::tfidf::Term;
+    ///
+    /// let tfidf = TfIdf::new();
+    /// tfidf.count(&Term("hello"));
+    /// ```
+    pub fn count(&self, term: &'a Term) -> i32 {
+        let mut count: i32 = 0;
 
+        for document in &self.documents {
+            let inner_count = document.into_iter().filter(
+                |&tx| tx == term
+            ).count();
+
+            if inner_count > 0 {
+                count += 1;
+            }
+        }
+
+        count
+    }
+
+    /// Returns the IDF of a specific Term
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// use tfidf::tfidf::Term;
+    ///
+    /// let mut tfidf = TfIdf::new();
+    /// tfidf.idf(&Term("hello"));
+    /// ```
+    pub fn idf(&self, term: &Term) -> f32 {
+        let docs = &self.documents;
+        let _idf: f32 = docs.len() as f32 / (1f32 + self.count(term) as f32);
+
+        _idf.log10()
+    }
+
+    /// Returns the TF of a specific Term
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// use tfidf::tfidf::Term;
+    ///
+    /// let mut tfidf = TfIdf::new();
+    /// tfidf.add("hello world");
+    /// tfidf.tf(&Term("hello"), 0);
+    /// ```
     pub fn tf(&self, term: &'a Term, document_index: usize) -> f32 {
         let ref document: Vec<Term<'a>> = self.documents[document_index];
 
         let counts: f32 = document.into_iter().filter(
-            |dx| dx.0 == term.0
+            |&dx| dx == term
         ).count() as f32;
 
         return match counts {
@@ -57,5 +135,21 @@ impl<'a> TfIdf<'a> {
                 counts.log10() + 1.0f32
             }
         };
+    }
+
+    /// Returns a specific TF-IDF of a Term
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tfidf::tfidf::TfIdf;
+    /// use tfidf::tfidf::Term;
+    ///
+    /// let mut tfidf = TfIdf::new();
+    /// tfidf.add("hello world");
+    /// tfidf.tfidf(&Term("hello"), 0);
+    /// ```
+    pub fn tfidf(&self, term: &'a Term, document_index: usize) -> f32 {
+        &self.tf(term, document_index) * &self.idf(term)
     }
 }
